561f37c4e9397c2c5fff51c73950d04255beda37
Nadia27 <jones.nadia.l@gmail.com>
2018-04-25 21:35:24 -0400

    Folder Tree

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
+import Cards from "./components/Cards/Cards";
 
 
 
@@ -12,6 +13,7 @@ class App extends Component {
   <Wrapper>
     <Header />
     <Jumbotron />
+          <Cards image={.image}/> 
   </Wrapper> 
   </div>
     );
